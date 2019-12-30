e554e6c4cb3905df33f60029dd71025b58ea9efc
jinyeong <jinyp91@gmail.com>
2019-12-30 19:00:46 +0900

    탭 뷰 컨트롤러 추가

--- PlantHabit/SecondViewController.swift
+++ PlantHabit/SecondViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var isZoom = false
+    var imgSeoul: UIImage?
+    var imgBusan: UIImage?
+    
+    @IBOutlet var mainImg: UIImageView!
+    @IBOutlet var resizeBtn: UIButton!
+    @IBOutlet var descLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgSeoul = UIImage(named: "seoul.jpg")
+        imgBusan = UIImage(named: "busan.jpg")
+        
+        mainImg.image = imgSeoul
     }
 
-
+    @IBAction func resizeBtn(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) {
+            newWidth = mainImg.frame.width/scale
+            newHeight = mainImg.frame.height/scale
+            resizeBtn.setTitle("확대", for: .normal)
+        } else {
+            newWidth = mainImg.frame.width*scale
+            newHeight = mainImg.frame.height*scale
+            resizeBtn.setTitle("축소", for: .normal)
+        }
+        mainImg.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func imgOnOffSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            mainImg.image = imgSeoul
+            descLbl.text = "Seoul"
+        } else {
+            mainImg.image = imgBusan
+            descLbl.text = "Busan"
+        }
+    }
 }
 
---
+++ PlantHabit/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  PlantHabit
+//
+//  Created by 박진영 on 2019/12/29.
+//  Copyright © 2019 Jinyeong Park. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
